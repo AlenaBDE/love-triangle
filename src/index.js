928b9b57d908e25ee93da895b128de9e7079e3db
AlenaBDE <bde.alena@gmail.com>
2018-02-25 17:37:38 +0300

    'solutions'

--- src/index.js
+++ src/index.js
@@ -2,6 +2,29 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+
+module.exports = function getLoveTrianglesCount(preferences) {
+    var aa = preferences.slice();
+    var count = 0;
+    var a, b, c;
+
+    for (var i = 0; i < preferences.length; i++) {
+
+        a = -1;
+        b = -1;
+        c = -1;
+
+        if ((aa[i] != -1) && (preferences[i] > 0)) var a = preferences[i];
+        if ((aa[a - 1] != -1) && (preferences[a - 1] > 0)) var b = preferences[a - 1];
+        if ((aa[b - 1] != -1) && (preferences[b - 1] > 0)) var c = preferences[b - 1];
+
+        if ((c - 1 == i) && (a != b && b != c) && (a > 0 && b > 0 && c > 0 )) {
+            aa[a - 1] = -1;
+            aa[b - 1] = -1;
+            aa[c - 1] = -1;
+            count++;
+        }
+
+    }
+    return count;
 };
